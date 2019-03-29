5f42c1d184942c29e1c5b4b30d844f3fd4070aa3
ryo <45418525+ryo-1-1-0-1@users.noreply.github.com>
2019-03-29 10:53:10 +0900

    Update config.php

--- config/config.php
+++ config/config.php
@@ -3,8 +3,8 @@
 ini_set('display_errors', 1);
 
 define('DSN', 'mysql:host=localhost;dbname=linedb;charset=utf8');
-define('DB_USERNAME', 'ryoto');
-define('DB_PASSWORD', '9048');
+define('DB_USERNAME', 'user_name');
+define('DB_PASSWORD', 'user_password');
 
 define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST']);
 
